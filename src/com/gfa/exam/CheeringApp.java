d7f02f75617250a4085bc1afcc95154f7175bc8b
varta5 <varga.tamas.pro@gmail.com>
2022-06-14 13:33:49 +0200

    c1

---
+++ src/com/gfa/exam/CheeringApp.java
@@ -0,0 +1,11 @@
+package com.gfa.exam;
+
+public class CheeringApp {
+    public static void main(String[] args) {
+
+    }
+
+    public static void cheer() {
+        System.out.println("Varga, Tamás");
+    }
+}
