3c94e6150bacdda5c9e4aa26f91fc210b2afb388
nyi zsolt <zsolt.nyiri1@gmail.com>
2022-06-14 13:58:12 +0200

    a6

--- src/com/gfa/exam/CheeringApp.java
+++ src/com/gfa/exam/CheeringApp.java
@@ -3,6 +3,9 @@ package com.gfa.exam;
 public class CheeringApp {
     public static void main(String[] args) {
 
+        public String baseLanguage = "tirpák";
+
+        cheer(baseLanguage);
     }
 
     public static void cheer(String baseLanguage) {
