49fec38dc34742ff0b43261e19fab1f398fd23b9
nyi zsolt <zsolt.nyiri1@gmail.com>
2022-06-14 13:29:54 +0200

    ai

---
+++ src/com/gfa/exam/Main.java
@@ -0,0 +1,13 @@
+package com.gfa.exam;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        greet();
+    }
+
+    public void greet(){
+        System.out.println("Zsolt");
+}
+}
