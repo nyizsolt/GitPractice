31e65aad6e0cb10d9961a1988665a6606b55a4a0
Tamas Solymosi <tamssolymosi@yahoo.com>
2022-06-14 13:33:19 +0200

    b1

---
+++ src/com/gfa/exam/count.java
@@ -0,0 +1,11 @@
+package com.gfa.exam;
+
+public class count {
+    public static void main(String[] args) {
+        count();
+    }
+
+    public static void count() {
+        System.out.printf("37");
+    }
+}
